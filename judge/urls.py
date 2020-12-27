4939ef70647ea0d5d2e0adacc816872687c552bd
GeovanaRamos <geovannna.1998@gmail.com>
2020-12-27 10:17:00 -0300

    Creating question application's view, template and styles

--- judge/urls.py
+++ judge/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from judge import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='judge/home.html'))
+    path('', TemplateView.as_view(template_name='judge/home.html'), name='home'),
+    path('applications/', views.QuestionListApplicationList.as_view(), name='question_list_application_list')
 ]
--- judge/views.py
+++ judge/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from judge import models
 
-# Create your views here.
+
+class QuestionListApplicationList(ListView):
+    model = models.QuestionListApplication
+    template_name = 'judge/question_list_application_list.html'
+
+    def get_queryset(self):
+        return models.QuestionListApplication.objects.filter(
+            course_class__in=self.request.user.get_classes())
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super(QuestionListApplicationList, self).get_context_data(**kwargs)
+
+        if hasattr(self.request.user, 'student'):
+            for application in data['object_list']:
+                application.concluded = application.student_has_concluded(self.request.user.student)
+
+        return data
