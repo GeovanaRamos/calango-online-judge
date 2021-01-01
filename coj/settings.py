4ec41fab34c7982c52e4b8008025fffffbfab0a3
GeovanaRamos <geovannna.1998@gmail.com>
2021-01-01 11:43:04 -0300

    Adding Login Required middleware

--- coj/settings.py
+++ coj/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'judge.middlewares.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'coj.urls'
